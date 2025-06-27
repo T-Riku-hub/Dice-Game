a17dd307ff9f0a6f82e70a235003bf89a0dc65c9
T-Riku-hub <rtoku9191@gmail.com>
2025-06-27 12:22:42 +0900

    Won/lost message modified

--- diceGame.py
+++ diceGame.py
@@ -1,4 +1,8 @@
 import random
+
+print("What is your name?")
+name = input("> ")
+print(f'Hello, {name}!')
 print("Rolling dice...")
 a = random.randint(1,6)
 b = random.randint(1,6)
@@ -7,6 +11,6 @@ print(f'Die 1: {a}')
 print(f'Die 2: {b}')
 print(f'Total value : {total}')
 if(total>=7):
-    print("You won!")
+    print(f"{name} won!")
 else:
-    print("You lost")
+    print(f"{name} lost")
